9d38eb28196474b72a5081a04c524652aabeb379
Ing Wilson <nosliwsys@gmail.com>
2019-04-07 15:15:22 -0500

    iniciando

---
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/tema5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a952000243.ingwilson.nosliwsys.tema5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
