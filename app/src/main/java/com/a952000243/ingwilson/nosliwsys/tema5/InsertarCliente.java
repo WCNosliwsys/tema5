e692e9ea77186505cff90f69f03002a2b0186789
Ing Wilson <nosliwsys@gmail.com>
2019-04-07 19:23:50 -0500

    finnnnnn

---
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/tema5/InsertarCliente.java
@@ -0,0 +1,66 @@
+package com.a952000243.ingwilson.nosliwsys.tema5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class InsertarCliente extends AppCompatActivity {
+
+    private EditText apellido;
+    private Spinner sexo;
+    private EditText nombre;
+    private EditText telefono;
+    private EditText direccion;
+    private int isexo;
+    HttpURLConnection conexion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar_cliente);
+        nombre = (EditText) findViewById(R.id.nombre);
+        direccion = (EditText) findViewById(R.id.direccion);
+        telefono = (EditText) findViewById(R.id.telefono);
+        apellido = (EditText) findViewById(R.id.apellido);
+        sexo = (Spinner) findViewById(R.id.sexo);
+        isexo=(sexo.getSelectedItem().toString().equals("Masculino"))?0:1;
+    }
+    public void Insertar(View view){
+        try {
+            String miurl= this.getString(R.string.dominio)+this.getString(R.string.insertarcliente)
+                    + "nombre="+ URLEncoder.encode(nombre.getText().toString(),"UTF-8") + "&apellido="
+                    + URLEncoder.encode(apellido.getText().toString(), "UTF-8") + "&sexo=" + isexo+"&telefono="
+                    + nombre.getText().toString()+ "&telefono="+ URLEncoder.encode(telefono.getText().toString(), "UTF-8")
+                    + "&direccion="+ URLEncoder.encode(direccion.getText().toString(), "UTF-8");
+            URL url=new URL(miurl);
+            Log.e("mierror",miurl);
+            conexion = (HttpURLConnection) url
+                    .openConnection();
+            if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+                String linea = reader.readLine();
+                if (!linea.equals("OK\\n")) {
+                    Toast.makeText(this,"se guardo",Toast.LENGTH_LONG);
+                    Log.e("mierror","Error en servicio Web nueva");
+                }
+                else
+                {Log.e("mierror","No hay error");}
+            } else {
+                Log.e("mierror", conexion.getResponseMessage());
+            }
+        } catch (Exception e) {
+            Log.e("mierror", e.getMessage(), e);
+        } finally {
+            if (conexion!=null) conexion.disconnect();
+        }
+    }
+}
---
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/tema5/MiNuevoAdaptador.java
@@ -0,0 +1,46 @@
+package com.a952000243.ingwilson.nosliwsys.tema5;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Vector;
+
+public class MiNuevoAdaptador  extends
+        RecyclerView.Adapter<MiNuevoAdaptador.ViewHolder> {
+    private LayoutInflater inflador;
+    private Vector<String> lista;
+    public MiNuevoAdaptador(Context context, Vector<String> lista) {
+        this.lista = lista;
+        inflador = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = inflador.inflate(R.layout.minuevoitem, parent, false);
+        return new ViewHolder(v);
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int i) {
+        holder.titulo.setText(lista.get(i));
+        holder.subtitutlo.setText(String.valueOf(i));
+    }
+    @Override
+    public int getItemCount() {
+        return lista.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView titulo, subtitutlo;
+        public ImageView icon;
+        ViewHolder(View itemView) {
+            super(itemView);
+            titulo = (TextView)itemView.findViewById(R.id.titulo);
+            subtitutlo = (TextView)itemView.findViewById(R.id.subtitulo);
+            icon = (ImageView)itemView.findViewById(R.id.icono);
+        }
+    }
+}
